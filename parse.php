89184c6973d8801abcf17841cbab0224358a1b6d
Chris Trumbull <chris@vividfront.com>
2016-04-27 14:08:53 -0400

    Initial Commit

--- parse.php
+++ parse.php
@@ -3,53 +3,52 @@
 class CSVParse
 {
 
-  private $handle;
-
-  public function __construct($handle)
+  public function __construct($handle, $del = ',')
   {
     $this->handle = $handle;
+    $this->del    = $del;
   }
 
   public function in_array_r($needle, $haystack)
   {
-    foreach ($haystack as $item) {
-        if($item['sku'] == $needle) {
-          return true;
-        }
-    }
 
+    foreach ($haystack as $item)
+    {
+      if($item['sku'] == $needle) {
+        return true;
+      }
+    }
     return false;
   }
 
-  public function DataToArray($delimiter=',')
+  public function DataToArray()
   {
-    $handle = $this->handle;
 
-  	if(!file_exists($handle) || !is_readable($handle))
-  		return false;
+    if(!file_exists($this->handle) || !is_readable($this->handle))
+      return false;
 
-  	$header = NULL;
-  	$data = array();
-  	if (($handle = fopen($handle, 'r')) !== FALSE)
-  	{
-  		while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
-  		{
-  			if(!$header)
-  				$header = $row;
-  			else
-  				$data[] = array_combine($header, $row);
-  		}
-  		fclose($handle);
-  	}
-  	return $data;
+    $header = NULL;
+    $data = array();
+    if (($this->handle = fopen($this->handle, 'r')) !== FALSE)
+    {
+      while (($row = fgetcsv($this->handle, 1000, $this->del)) !== FALSE)
+      {
+        if(!$header)
+          $header = $row;
+        else
+          $data[] = array_combine($header, $row);
+        }
+    fclose($this->handle);
+    }
+    return $data;
   }
 
   public function CreateConfigurable()
   {
+
     $rows = $this->DataToArray();
     $data = array();
     $counts = array();
-
     foreach($rows as $row)
     {
 
@@ -59,22 +58,19 @@ class CSVParse
         $sku = $row['SKU'];
       }
 
-    	if(!$this->in_array_r($sku, $data)) {
-    		$data[] =
-    						array(
-    							'sku' =>  $sku,
-    							'name' => $row['Name'],
+      if(!$this->in_array_r($sku, $data)) {
+        $data[] =
+                array(
+                  'sku' =>  $sku,
+                  'name' => $row['Name'],
                   'configurable' => NULL
-    						);
-
-    	}
+                );
+      }
 
     }
 
     foreach ($data as $item => $value) {
-
       $i = 0;
-
       foreach ($rows as $row) {
         if(strpos($row['SKU'], $data[$item]['sku']) !== FALSE)
         {
@@ -91,25 +87,24 @@ class CSVParse
   public function DataToCsv()
   {
     $data = array();
-
     $rows = $this->DataToArray();
     foreach($rows as $row)
     {
       $sku = substr($row['SKU'], 0, strpos($row['SKU'], '-'));
 
-    	if(!$this->in_array_r($sku, $data)) {
-    		$data[] =
-    						array(
-    							'sku' => strtolower(trim($sku)),
-    							'name' => $row['Name'],
+      if(!$this->in_array_r($sku, $data))
+      {
+        $data[] =
+                array(
+                  'sku' => strtolower(trim($sku)),
+                  'name' => $row['Name'],
                   'configurable' => $row['configurable']
-    						);
-
-    	}
-
+                );
+      }
     }
     return $data;
   }
+
 }
 
   $csv = new CSVParse("test.csv");
